7f0a7bfdbe7cf6a1ebd4d3942fb679e704a5ed34
Nathan Harding <nathanharding02@gmail.com>
2022-02-23 17:19:38 -0700

    Added buttons for project/task creation, and page view

--- src/index.js
+++ src/index.js
@@ -14,36 +14,116 @@ function createHeader() {
     pageName.textContent = "Todo List";
 
     header.appendChild(pageName);
+    header.appendChild(createNav());
 
     return header;
 }
 
-/* function createHome() {
-    const home = document.createElement("div");
-    home.classList.add("home");
+function createNav() {
+    const nav = document.createElement("nav");
 
-    return home;
+    const allButton = document.createElement("button");
+    allButton.classList.add("button-nav");
+    allButton.textContent = "All Tasks";
+    allButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(allButton);
+    });
+
+    const todayButton = document.createElement("button");
+    todayButton.classList.add("button-nav");
+    todayButton.textContent = "Today's Tasks";
+    todayButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(todayButton);
+        loadToday();
+    });
+
+    const weekButton = document.createElement("button");
+    weekButton.classList.add("button-nav");
+    weekButton.textContent = "This Week's Tasks";
+    weekButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(weekButton);
+    });
+
+    nav.appendChild(allButton);
+    nav.appendChild(todayButton);
+    nav.appendChild(weekButton);
+
+    return nav;
 }
 
-function loadHome() {
+function setActiveButton(button) {
+    const buttons = document.querySelectorAll(".button-nav");
+
+    buttons.forEach((button) => {
+        if (button !== this) {
+            button.classList.remove("active");
+        }
+    });
+
+    button.classList.add("active");
+}
+
+function createToday() {
+    const today = document.createElement("div");
+    today.classList.add("today-tasks");
+    Storage.getTodoList()
+        .getProjects()
+        .forEach((project) => {
+            if (
+                project.name !== "Inbox" &&
+                project.name !== "Today" &&
+                project.name !== "This week"
+            ) {
+                createProject(project.name);
+            }
+        });
+
+    return today;
+}
+
+function loadToday() {
     const main = document.getElementById("main");
     main.textContent = "";
-    main.appendChild(createHome());
-} */
+    main.appendChild(createToday());
+
+
+}
 
 function createMain() {
     const main = document.createElement("main");
     main.classList.add("main");
     main.setAttribute("id", "main");
+
+    const createProjectButton = document.createElement("button");
+    createProjectButton.classList.add("button-main");
+    createProjectButton.textContent = "Create Project";
+    createProjectButton.addEventListener("click", (e) => {
+        createProject();
+    });
+
+    const createTaskButton = document.createElement("button");
+    createTaskButton.classList.add("button-main");
+    createTaskButton.textContent = "Create Task";
+    createTaskButton.addEventListener("click", (e) => {
+        createTask();
+    });
+
+    main.appendChild(createProjectButton);
+    main.appendChild(createTaskButton);
     return main;
 }
 
 function loadPage() {
     const page = document.getElementById("content");
     page.appendChild(createHeader());
-    //page.appendChild(createMain());
+    page.appendChild(createMain());
 
-    //loadHome();
+    setActiveButton(document.querySelector(".button-nav"));
+
+    //loadToday();
 }
 
 loadPage();
